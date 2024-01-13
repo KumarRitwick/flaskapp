d5b466a260a2657d7ac55429826759f671eea28e
Ritwick Kumar <58397038+KumarRitwick@users.noreply.github.com>
2024-01-13 22:20:57 +0000

    Update catalogue.py

--- catalogue.py
+++ catalogue.py
@@ -1,86 +1,73 @@
-from datetime import *
-import time
-import sys
-
+from flask import Flask, render_template, redirect, url_for
 import json
 import requests
-# First we set our credentials
 
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/Video/<video>')
 def video_page(video):
-    print (video)
-    url = 'http://34.154.76.75/myflix/videos?filter={"video.uuid":"'+video+'"}'
+    url = 'http://34.154.15.243/myflix/videos?filter={"video.uuid":"' + video + '"}'
     headers = {}
-    payload = json.dumps({ })
-    print (request.endpoint)
+    payload = json.dumps({})
     response = requests.get(url)
-    print (url)
+    
     if response.status_code != 200:
-      print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
-      return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
+        return "Unexpected response: {0}. Status: {1}. Message: {2}".format(
+            response.reason, response.status, response.text
+        )
+
     jResp = response.json()
-    print (type(jResp))
-    print (jResp)
     for index in jResp:
         for key in index:
-           if (key !="_id"):
-              print (index[key])
-              for key2 in index[key]:
-                  print (key2,index[key][key2])
-                  if (key2=="Name"):
-                      video=index[key][key2]
-                  if (key2=="file"):
-                      videofile=index[key][key2]
-                  if (key2=="pic"):
-                      pic=index[key][key2]
-    return render_template('video.html', name=video,file=videofile,pic=pic)
+            if key != "_id":
+                for key2 in index[key]:
+                    if key2 == "Name":
+                        video_name = index[key][key2]
+                    if key2 == "file":
+                        video_file = index[key][key2]
+                    if key2 == "pic":
+                        video_pic = index[key][key2]
+
+    return render_template('video.html', name=video_name, file=video_file, pic=video_pic)
 
 @app.route('/')
 def cat_page():
-    url = "http://34.154.76.75/myflix/videos"
+    url = "http://35.204.223.27/myflix/videos"
     headers = {}
-    payload = json.dumps({ })
+    payload = json.dumps({})
 
     response = requests.get(url)
-    #print (response)
-    # exit if status code is not ok
-    print (response)
-    print (response.status_code)
-    if response.status_code != 200:
-      print("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message']))
-      return "Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, jResp['Exception']['Message'])
-    jResp = response.json()
-    print (type(jResp))
-    html="<h2> Your Videos</h2>"
-    for index in jResp:
-       #print (json.dumps(index))
-       print ("----------------")
-       for key in index:
 
-           if (key !="_id"):
-              print (index[key])
-              for key2 in index[key]:
-                  print (key2,index[key][key2])
-                  if (key2=="Name"):
-                      name=index[key][key2]
-                  if (key2=="thumb"):
-                      thumb=index[key][key2]
-                  if (key2=="uuid"):
-                      uuid=index[key][key2]  
-              html=html+'<h3>'+name+'</h3>'
-              ServerIP=request.host.split(':')[0]
-              html=html+'<a href="http://'+ServerIP+'/Video/'+uuid+'">'
-              html=html+'<img src="http://34.154.76.75/pics/'+thumb+'">'
-              html=html+"</a>"        
-              print("=======================")
+    if response.status_code != 200:
+        return "Unexpected response: {0}. Status: {1}. Message: {2}".format(
+            response.reason, response.status, response.text
+        )
+
+    jResp = response.json()
+    html = "<h2>Your Videos</h2>"
+
+    if jResp:
+        first_video_uuid = jResp[0]['videos']['file']
+        return redirect(url_for('video_page', video=first_video_uuid))
+
+    for index in jResp:
+        for key in index:
+            if key != "_id":
+                for key2 in index[key]:
+                    if key2 == "Name":
+                        name = index[key][key2]
+                    if key2 == "thumb":
+                        thumb = index[key][key2]
+                    if key2 == "file":
+                        uuid = index[key][key2]
+
+                html = html + '<h3>' + name + '</h3>'
+                html = html + '<a href="' + url_for('video_page', video=uuid) + '">'
+                html = html + '<img src="http://35.204.223.27/pics/' + thumb + '">'
+                html = html + "</a>"
 
     return html
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port="5000")
+    app.run(host='0.0.0.0', port="5000")
